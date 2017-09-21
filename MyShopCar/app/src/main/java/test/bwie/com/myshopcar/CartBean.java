ffa857a898f6d3932c879c91d063c29c90f8959f
zhaoshanshan0906 <2410002148@qq.com>
2017-09-21 14:29:26 +0800

    2017-09-21购物车

---
+++ MyShopCar/app/src/main/java/test/bwie/com/myshopcar/CartBean.java
@@ -0,0 +1,15 @@
+package test.bwie.com.myshopcar;
+
+/**
+ * Created by louisgeek on 2016/4/27.
+ */
+public class CartBean {
+
+    /** 商品ID */
+    private String goodsid;
+    /** 店铺ID */
+    private String storeid;
+
+
+
+}
---
+++ MyShopCar/zssxiangmu/CartShopping2/app/src/main/java/test/bwie/com/cartshopping/ToastUtil.java
@@ -0,0 +1,22 @@
+package test.bwie.com.cartshopping;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * 提醒工具
+ * 
+ * @author WeiGong
+ * 
+ */
+
+public class ToastUtil {
+
+	public static void showL(Context context, String info) {
+		Toast.makeText(context, info, Toast.LENGTH_LONG).show();
+	}
+
+	public static void showS(Context context, String info) {
+		Toast.makeText(context, info, Toast.LENGTH_SHORT).show();
+	}
+}
